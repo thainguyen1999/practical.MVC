b5a28b6b12656e9b6e21790e1dd79f065616549c
thainguyen1999 <113773228+thainguyen1999@users.noreply.github.com>
2023-05-26 18:26:40 +0700

    Add project files.

---
+++ practical.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practical.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
